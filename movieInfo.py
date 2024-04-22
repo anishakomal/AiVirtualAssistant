15d133703b82fddd95bee27d56499e6996f66c60
anisha <anishakomal2001@gmail.com>
2024-04-22 19:25:06 +0530

    commit movieInfo.py

--- movieInfo.py
+++ movieInfo.py
@@ -33,64 +33,66 @@ def takeCommand():
 
 
 import requests
+def moviesInfo():
+    # Replace 'YOUR_API_KEY' with your actual OMDb API key
+    api_key = '4a2a54e7'
 
-# Replace 'YOUR_API_KEY' with your actual OMDb API key
-api_key = '4a2a54e7'
+    # Base URL for OMDb API
+    base_url = 'http://www.omdbapi.com/'
 
-# Base URL for OMDb API
-base_url = 'http://www.omdbapi.com/'
+    # Movie title to search for
+    speak('Enter Movie Name:')
+    movie_title = input()
 
-# Movie title to search for
-speak('Enter Movie Name:')
-movie_title = input()
+    # Construct the API request URL
+    url = f'{base_url}?apikey={api_key}&t={movie_title}'
 
-# Construct the API request URL
-url = f'{base_url}?apikey={api_key}&t={movie_title}'
+    # Send GET request to the API
+    response = requests.get(url)
 
-# Send GET request to the API
-response = requests.get(url)
+    # Check if the request was successful
+    if response.status_code == 200:
+        # Parse JSON response
+        movie_data = response.json()
 
-# Check if the request was successful
-if response.status_code == 200:
-    # Parse JSON response
-    movie_data = response.json()
+        # Display movie information
+        title = movie_data['Title']
+        year = movie_data['Year']
+        summaries = movie_data['Plot']
+        # rating = movie_data['imdbRating']
 
-    # Display movie information
-    title = movie_data['Title']
-    year = movie_data['Year']
-    summaries = movie_data['Plot']
-    # rating = movie_data['imdbRating']
+        # print('Title:', title)
+        # speak('Title is {title}' )
 
-    # print('Title:', title)
-    # speak('Title is {title}' )
+        # print('Year:', movie_data['Year'])
+        # print('Year:', year)
+        # speak(year)
 
-    # print('Year:', movie_data['Year'])
-    # print('Year:', year)
-    # speak(year)
+        # print('Plot:', summaries)
+        # speak(summaries)
 
-    # print('Plot:', summaries)
-    # speak(summaries)
+        # Display IMDb rating
+        # print('IMDb Rating:', movie_data['imdbRating'])
+        # You can access more information from the 'movie_data' dictionary
 
-    # Display IMDb rating
-    # print('IMDb Rating:', movie_data['imdbRating'])
-    # You can access more information from the 'movie_data' dictionary
+        # Access more information from the 'movie_data' dictionary
+        print('Title:', movie_data['Title'])
+        print('Year:', movie_data['Year'])
+        print('Rated:', movie_data['Rated'])
+        print('Released:', movie_data['Released'])
+        print('Runtime:', movie_data['Runtime'])
+        print('Genre:', movie_data['Genre'])
+        print('Director:', movie_data['Director'])
+        print('Writer:', movie_data['Writer'])
+        print('Actors:', movie_data['Actors'])
 
-    # Access more information from the 'movie_data' dictionary
-    print('Title:', movie_data['Title'])
-    print('Year:', movie_data['Year'])
-    print('Rated:', movie_data['Rated'])
-    print('Released:', movie_data['Released'])
-    print('Runtime:', movie_data['Runtime'])
-    print('Genre:', movie_data['Genre'])
-    print('Director:', movie_data['Director'])
-    print('Writer:', movie_data['Writer'])
-    print('Actors:', movie_data['Actors'])
+        print('Language:', movie_data['Language'])
+        print('Country:', movie_data['Country'])
+        print('Awards:', movie_data['Awards'])
+        print('Poster URL:', movie_data['Poster'])
+        print('IMDb Rating:', movie_data['imdbRating'])
+        print('Plot:', movie_data['Plot'])
+    else:
+        print('Error:', response.status_code)
 
-    print('Language:', movie_data['Language'])
-    print('Country:', movie_data['Country'])
-    print('Awards:', movie_data['Awards'])
-    print('Poster URL:', movie_data['Poster'])
-    print('IMDb Rating:', movie_data['imdbRating'])
-    print('Plot:', movie_data['Plot'])
-else:
-    print('Error:', response.status_code)
+moviesInfo()
