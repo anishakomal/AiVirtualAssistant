371740dc5fbf95dc7fa3f1455fe3621ee91cb9f7
amisha12-kumari <amishakumari2825@gmail.com>
2024-04-22 19:01:26 +0530

    set alarm

--- alarm.py
+++ alarm.py
@@ -1,33 +1,62 @@
-import os
-from playsound import playsound
+'''
+#Function
+def alarm(query):
+    timehere = open("Alarmtext.txt", "a")
+    timehere.write(query)
+    timehere.close()
+    os.startfile("alarm1.py")
+      
+    
+     
+    elif "set an alarm" in query:
+            print("input time example:- 10 and 10")
+            speak("Set the time")
+            a = input("Please tell the time :-")
+            alarm(a)
+            speak("Done,sir")
+
+        elif "the time" in query:
+            strTime = datetime.datetime.now().strftime("%H:%M")
+            # speak(f"Sir,the time is {strTime}")
+'''
+
+import pyttsx3
 import datetime
+import os 
 
-extracted_time = open("C:\\Users\\Administrator\\PycharmProjects\\AiVirtualAssistant\\Data.txt", 'rt')
-time = extracted_time.read()
+engine = pyttsx3.init("sapi5")
+voices = engine.getProperty("voices")
+engine.setProperty("voice", voices[0].id)
+engine.setProperty("rate",200)
+
+def speak(audio):
+    engine.say(audio)
+    engine.runAndWait()
+
+extractedtime = open("Alarmtext.txt","rt")
+time = extractedtime.read()
 Time = str(time)
+extractedtime.close()
 
-delete_time = open("C:\\Users\\Administrator\\PycharmProjects\\AiVirtualAssistant\\Data.txt", 'r+')
-delete_time.truncate(0)
-delete_time.close()
+deletetime = open("Alarmtext.txt","r+")
+deletetime.truncate(0)
+deletetime.close()
 
-def RingerNow(time):
+ 
 
-    time_to_set = str(time)
-    time_now = time_to_set.replace("jarvis","")
-    time_now = time_now.replace("set alarm for","")
-    time_now = time_now.replace("set", "")
-    time_now = time_now.replace("alarm", "")
-    time_now = time_now.replace("for", "")
-    time_now = time_now.replace(" and " ,  ":")
-
-    Alarm_Time = str(time_now)
-
+def ring(time):
+    timeset = str(time)
+    timenow = timeset.replace("jarvis","")
+    timenow = timenow.replace("set an alarm","")
+    timenow = timenow.replace(" and ",":")
+    Alarmtime = str(timenow)
+    print(Alarmtime)
     while True:
-        current_time=datetime.datetime.now().strftime("%H:%M")
+        currenttime = datetime.datetime.now().strftime("%H:%M")
+        if currenttime == Alarmtime:
+            speak("Alarm ringing,sir")
+            os.startfile("notification.mp3") #You can choose any music or ringtone 
+        elif currenttime + "00:00:30" == Alarmtime:
+            exit()
 
-        if current_time == Alarm_Time:
-            print("Wake Up Madam,its Time To Work")
-            playsound("C:\\Users\\Administrator\\PycharmProjects\\AiVirtualAssistant\\Database\\Sounds\\notification.mp3")
-
-
-RingerNow(Time)
+ring(time)
